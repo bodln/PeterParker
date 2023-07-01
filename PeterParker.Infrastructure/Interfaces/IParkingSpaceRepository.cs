3a5c12f29980613a1dd047f8322d6e81a9f134cf
Ramiz Šabović <frostywinterr@gmail.com>
2023-07-01 12:30:33 +0200

    Add Repository Interfaces for all Models.

---
+++ PeterParker.Infrastructure/Interfaces/IParkingSpaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using PeterParker.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterParker.Infrastructure.Interfaces
+{
+    internal interface IParkingSpaceRepository : IRepository<ParkingSpace>
+    {
+    }
+}
---
+++ PeterParker.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterParker.Infrastructure.Interfaces
+{
+    internal interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
