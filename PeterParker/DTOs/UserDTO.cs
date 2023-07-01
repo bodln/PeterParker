be7c4a8f44c4037402018be5c925ab5a1455e26c
Ramiz Šabović <frostywinterr@gmail.com>
2023-07-01 14:36:59 +0200

    Add Initial User Controller and UserDTO.

---
+++ PeterParker/DTOs/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using PeterParker.Data.Models;
+
+namespace PeterParker.DTOs
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public List<string> Vehicles { get; set; } = new List<string>();
+    }
+}
