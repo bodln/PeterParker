e6b2910c77acc92cfd10a39e0288d0b14d2ff34b
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-04 22:40:40 +0200

    Move DTOs to PeterParker.Data to resolve referencing issues

---
+++ PeterParker.Data/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using PeterParker.Data.Models;
+
+namespace PeterParker.DTOs;
+
+public class UserDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public List<string> Vehicles { get; set; } = new List<string>();
+}
