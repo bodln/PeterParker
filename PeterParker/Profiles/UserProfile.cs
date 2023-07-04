8d4740f61efdcb06a0c51e5f83402396a8ed9369
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-04 22:41:19 +0200

    UserProfile for User AutoMapper configuration

---
+++ PeterParker/Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using PeterParker.Data.Models;
+using PeterParker.DTOs;
+
+namespace PeterParker.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDTO>();
+    }
+}
