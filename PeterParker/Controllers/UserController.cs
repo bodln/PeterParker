7e72b8b864ef3b95db5177ea88810a2288878b7e
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-05 18:30:25 +0200

    Add Register, Login and MakeAdmin endpoints

--- PeterParker/Controllers/UserController.cs
+++ PeterParker/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PeterParker.DTOs;
 using PeterParker.Infrastructure;
 using PeterParker.Infrastructure.Interfaces;
 
@@ -14,6 +17,41 @@ namespace PeterParker.Controllers
         public UserController(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser(UserDTO request)
+        {
+            IdentityResult result = unitOfWork.UserRepository.RegisterUser(request).Result;
+
+            if (result.Succeeded)
+            {
+                return Ok(request.Email + "Successfully Registered.");
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("LogIn")]
+        public async Task<string> LogInUser(UserDTO request)
+        {
+            return await unitOfWork.UserRepository.LogInUser(request);
+        }
+
+        [HttpPost("MakeAdmin")]
+        [Authorize("AdminOnly")]
+        public async Task<IActionResult> AddAdminRole(string request)
+        {
+            IdentityResult result = unitOfWork.UserRepository.AddAdminRole(request).Result;
+
+            if (result.Succeeded)
+            {
+                return Ok("User Successfully Made Admin");
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
         }
     }
 }
