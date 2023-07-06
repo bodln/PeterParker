8855df0327ed0eb7935ec7be222bd36f0c5c0e44
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-06 19:45:03 +0200

    Add mappin to and from VehicleDTO

--- PeterParker/Profiles/UserProfile.cs
+++ PeterParker/Profiles/UserProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using PeterParker.Data.DTOs;
 using PeterParker.Data.Models;
 using PeterParker.DTOs;
 
@@ -6,6 +7,12 @@ namespace PeterParker.Profiles;
 
 public class UserProfile : Profile
 {
+    private readonly DataContext context;
+
+    public UserProfile(DataContext context)
+    {
+        this.context = context;
+    }
     public UserProfile()
     {
         CreateMap<UserDTO, User>()
@@ -13,5 +20,15 @@ public class UserProfile : Profile
                 dest.UserName,
                 opt => opt.MapFrom(src => src.Email));
         CreateMap<User, UserDTO>();
+        CreateMap<VehicleDTO, Vehicle>()
+            .ForMember(dest =>
+                dest.User,
+                opt => opt.MapFrom(src => context!.Users.First(u => u.Email == src.UserEmail)) // <-- Doesn't work
+                );
+        CreateMap<Vehicle, VehicleDTO>()
+            .ForMember(dest =>
+                dest.UserEmail,
+                opt => opt.MapFrom(src => src.User.Email)
+                );
     }
 }
