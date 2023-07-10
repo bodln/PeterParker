fe8b9a0556f5cb3c3bab852be3ef26cd986b0d61
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-10 16:11:02 +0200

    Make UserDTO return a list of TicketDTOs

--- PeterParker.Data/DTOs/UserDTO.cs
+++ PeterParker.Data/DTOs/UserDTO.cs
@@ -11,4 +11,6 @@ public class UserDTO
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public List<VehicleDTO> Vehicles { get; set; } = new List<VehicleDTO>();
+    public List<TicketDTO> Tickets { get; set; } = new List<TicketDTO>();
+
 }
