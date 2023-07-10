64f1026e1ba2d9c847eef1b160e31795a348ad2c
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-07-10 16:10:32 +0200

    Add impromtu mapping for Ticket, Zone and ParkingSpace from their respective DTOs

--- PeterParker/Profiles/UserProfile.cs
+++ PeterParker/Profiles/UserProfile.cs
@@ -19,16 +19,46 @@ public class UserProfile : Profile
             .ForMember(dest =>
                 dest.UserName,
                 opt => opt.MapFrom(src => src.Email));
+        
         CreateMap<User, UserDTO>();
+        
         CreateMap<VehicleDTO, Vehicle>()
             .ForMember(dest =>
                 dest.User,
                 opt => opt.MapFrom(src => context!.Users.First(u => u.Email == src.UserEmail)) // <-- Doesn't work
                 );
+        
         CreateMap<Vehicle, VehicleDTO>()
             .ForMember(dest =>
                 dest.UserEmail,
                 opt => opt.MapFrom(src => src.User.Email)
                 );
+
+        CreateMap<TicketDTO, Ticket>()
+            .ForMember(dest =>
+                dest.Zone,
+                opt => opt.MapFrom(src => context.Zones.Where(z => z.Id == src.ZoneId))
+                )
+            .ForMember(dest =>
+                dest.ParkingSpace,
+                opt => opt.MapFrom(src => context.ParkingSpaces.Where(ps => ps.Id == src.ParkingSpaceId))
+                );
+
+        CreateMap<Ticket, TicketDTO>();
+
+        CreateMap<ZoneDTO, Zone>()
+            .ForMember(
+                dest =>
+                dest.ParkingSpaces, opt => opt.Ignore()
+            );
+
+        CreateMap<ParkingSpace, ParkingSpaceDTO>()
+            .ForMember(
+                dest =>
+                dest.VehicleRegistration, opt => opt.MapFrom(src => src.Vehicle.Registration)
+            ).ForMember(
+                dest =>
+                dest.GarageName, opt => opt.Ignore()
+            );
     }
 }
