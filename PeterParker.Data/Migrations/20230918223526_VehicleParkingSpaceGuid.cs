3818b4465ea85ae7c643564d9e445d4b7541b0fc
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-09-19 00:36:53 +0200

    New Vehicle migrations

---
+++ PeterParker.Data/Migrations/20230918223526_VehicleParkingSpaceGuid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeterParker.Data.Migrations
+{
+    public partial class VehicleParkingSpaceGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParkingSpaceGuid",
+                table: "Vehicles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParkingSpaceGuid",
+                table: "Vehicles");
+        }
+    }
+}
